1453c47d9337addbb2961b41c2cb2463ed92d96a
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2022-07-16 23:58:47 -0300

    Update PointerEnterHandler.cs

--- Runtime/Frameworks/UGUI/EventHandlers/PointerEnterHandler.cs
+++ Runtime/Frameworks/UGUI/EventHandlers/PointerEnterHandler.cs
@@ -22,7 +22,6 @@ namespace ReactUnity.UGUI.EventHandlers
             float scaleFactor = ReactEl?.Component?.Context?.RootCanvas?.scaleFactor ?? 1.0f;
 
             eventData.position = new Vector2(eventData.position.x / scaleFactor, eventData.position.y / scaleFactor);
-            eventData.pressPosition = new Vector2(eventData.pressPosition.x / scaleFactor, eventData.pressPosition.y / scaleFactor);
 
             OnEvent?.Invoke(eventData);
         }
