31c716dae1cce903ece5a87c25b21e2e5df8fbc5
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2022-07-17 00:13:24 -0300

    Update DragHandler.cs

--- Runtime/Frameworks/UGUI/EventHandlers/DragHandler.cs
+++ Runtime/Frameworks/UGUI/EventHandlers/DragHandler.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using ReactUnity.UGUI.Behaviours;
 
 namespace ReactUnity.UGUI.EventHandlers
 {
@@ -9,20 +8,9 @@ namespace ReactUnity.UGUI.EventHandlers
     public class DragHandler : MonoBehaviour, IDragHandler, IEventHandler
     {
         public event Action<BaseEventData> OnEvent = default;
-        
-        private ReactElement ReactEl;
-         
-        private void Start()
-        {
-            ReactEl = GetComponent<ReactElement>();
-        }
 
         public void OnDrag(PointerEventData eventData)
         {
-            float scaleFactor = ReactEl?.Component?.Context?.RootCanvas?.scaleFactor ?? 1.0f;
-
-            eventData.scrollDelta = new Vector2((eventData.pressPosition.x - eventData.position.x) / scaleFactor, (eventData.position.y - eventData.pressPosition.y) / scaleFactor);
-
             OnEvent?.Invoke(eventData);
         }
 
