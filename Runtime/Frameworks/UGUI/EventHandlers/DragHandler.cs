357a9951b2d6c349d27c9a66a37509b6f0359120
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2022-07-16 23:36:03 -0300

    Update DragHandler.cs

--- Runtime/Frameworks/UGUI/EventHandlers/DragHandler.cs
+++ Runtime/Frameworks/UGUI/EventHandlers/DragHandler.cs
@@ -21,10 +21,8 @@ namespace ReactUnity.UGUI.EventHandlers
         {
             float scaleFactor = ReactEl?.Component?.Context?.RootCanvas?.scaleFactor ?? 1.0f;
 
-            eventData.position = new Vector2(eventData.position.x / scaleFactor, eventData.position.y / scaleFactor);
-            eventData.pressPosition = new Vector2(eventData.pressPosition.x / scaleFactor, eventData.pressPosition.y / scaleFactor);
+            eventData.delta = new Vector2((eventData.position.x - eventData.pressPosition.x) / scaleFactor, (eventData.pressPosition.y - eventData.position.y) / scaleFactor);
 
-        
             OnEvent?.Invoke(eventData);
         }
 
