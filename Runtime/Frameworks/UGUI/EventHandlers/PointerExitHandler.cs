5f93e233f8ff7fdaaccb6ab444132e112764c9ef
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2022-09-10 06:00:39 -0300

    Update PointerExitHandler.cs

--- Runtime/Frameworks/UGUI/EventHandlers/PointerExitHandler.cs
+++ Runtime/Frameworks/UGUI/EventHandlers/PointerExitHandler.cs
@@ -5,13 +5,38 @@ using UnityEngine.EventSystems;
 namespace ReactUnity.UGUI.EventHandlers
 {
     [EventHandlerPriority(EventPriority.Continuous)]
-    public class PointerExitHandler : MonoBehaviour, IPointerExitHandler, IEventHandler
+    public class PointerExitHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IEventHandler
     {
         public event Action<BaseEventData> OnEvent = default;
+        
+        private bool IsHovered;
+        private PointerEventData LastEventData;
 
         public void OnPointerExit(PointerEventData eventData)
         {
             OnEvent?.Invoke(eventData);
+            
+            LastEventData = null;
+            
+            IsHovered = false;
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            IsHovered = true;
+            
+            LastEventData = eventData;
+        }
+        
+        private void OnDisable()
+        {
+            if(IsHovered) {
+                OnEvent?.Invoke(LastEventData);
+                
+                IsHovered = false;
+                
+                LastEventData = null;
+            }
         }
 
         public void ClearListeners()
